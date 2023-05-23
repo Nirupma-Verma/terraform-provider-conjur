7e51532250d0a7a5cbb6928989c543a1273b4ddb
Nirupma-Verma <113432409+Nirupma-Verma@users.noreply.github.com>
2023-05-23 10:57:46 +0530

    Update provider.go

--- conjur/provider.go
+++ conjur/provider.go
@@ -1,17 +1,12 @@
 package conjur
-
 import (
 	"crypto/sha256"
 	"encoding/hex"
 	"log"
-
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-
 	"github.com/cyberark/conjur-api-go/conjurapi"
 	"github.com/cyberark/conjur-api-go/conjurapi/authn"
 )
-
-// Provider implements Conjur as a schema.Provider
 func Provider() *schema.Provider {
 	return &schema.Provider{
 		DataSourcesMap: map[string]*schema.Resource{
@@ -52,56 +47,38 @@ func Provider() *schema.Provider {
 		ConfigureFunc: providerConfig,
 	}
 }
-
 func providerConfig(d *schema.ResourceData) (interface{}, error) {
-
 	config, err := conjurapi.LoadConfig()
 	if err != nil {
 		return nil, err
 	}
-
-	// If server info has been specified in the schema, use it. Otherwise,
-	// assume the environment has everything needed.
 	appliance_url := d.Get("appliance_url").(string)
 	if appliance_url != "" {
 		config.ApplianceURL = appliance_url
 	}
-	
-	// Add `/api` to appliance url for Conjur Cloud support
-	appliance_url += "/api"
-
 	account := d.Get("account").(string)
 	if account != "" {
 		config.Account = account
 	}
-
 	ssl_cert := d.Get("ssl_cert").(string)
 	if ssl_cert != "" {
 		config.SSLCert = ssl_cert
 	}
-
 	ssl_cert_path := d.Get("ssl_cert_path").(string)
 	if ssl_cert_path != "" {
 		config.SSLCertPath = ssl_cert_path
 	}
-
-	// If creds have been specified in the schema, use them. Otherwise,
-	// assume the environment has everything needed.
 	login := d.Get("login").(string)
 	apiKey := d.Get("api_key").(string)
 	if login != "" && apiKey != "" {
 		loginPair := authn.LoginPair{Login: login, APIKey: apiKey}
-
 		return conjurapi.NewClientFromKey(config, loginPair)
 	}
-
 	return conjurapi.NewClientFromEnvironment(config)
 }
-
 func dataSourceSecret() *schema.Resource {
 	return &schema.Resource{
 		Read: dataSourceSecretRead,
-
 		Schema: map[string]*schema.Schema{
 			"name": {
 				Type:        schema.TypeString,
@@ -123,27 +100,19 @@ func dataSourceSecret() *schema.Resource {
 		},
 	}
 }
-
 func dataSourceSecretRead(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*conjurapi.Client)
-
 	name := d.Get("name").(string)
 	version := d.Get("version").(string)
-
 	log.Printf("[DEBUG] Getting secret for name=%q version=%q", name, version)
-
 	secretValue, err := client.RetrieveSecret(name)
-
 	if err != nil {
 		return err
 	}
-
 	d.Set("value", string(secretValue))
 	d.SetId(hash(string(secretValue)))
-
 	return nil
 }
-
 func hash(s string) string {
 	sha := sha256.Sum256([]byte(s))
 	return hex.EncodeToString(sha[:])
